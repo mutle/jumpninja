614f5f14cdfc397042d750099852d52c1978af0f
Mutwin Kraus <mutwin.kraus@gmail.com>
2011-12-17 05:29:05 +0100

    Sprite and Text implementation

--- game.coffee
+++ game.coffee
@@ -7,28 +7,83 @@ $(document).ready ->
       @ctx = @screen.getContext "2d"
       @w = @screen.width
       @h = @screen.height
-      @ctx.fillColor = '#fff'
+      @clearColor = '#000'
+      @ctx.fillStyle = @clearColor
       @ctx.fillRect 0, 0, @w, @h
       @running = true
       @objects = []
+      @lastMillis = @milliseconds()
     add: (object) ->
       @objects.push object
     update: ->
       return if not @running
-      delta = 0
+      @currentMillis = @milliseconds()
+      delta = (@currentMillis - @lastMillis) / 1000
       for o in @objects
         o.update delta
+      @ctx.fillStyle = @clearColor
       @ctx.fillRect 0, 0, @w, @h
       for o in @objects
-        o.draw @ctx
+        o.draw this
+      @lastMillis = @currentMillis
     pause: ->
       @running = not @running
+      @lastMillis = @milliseconds()
+    translate: (offset, rotate, callback) ->
+      angle = rotate * Math.PI / 180
+      @ctx.translate offset.x, offset.y
+      @ctx.rotate angle
+      callback.apply this
+      @ctx.rotate -angle
+      @ctx.translate -offset.x, -offset.y
+
+    drawImage: (image, src, dst, rotate, center) ->
+      @translate dst.pos, rotate, ->
+        @ctx.drawImage image, src.pos.x, src.pos.y, src.size.x, src.size.y, -center.x, -center.y, dst.size.x, dst.size.y
+    measureText: (text, color, font, align) ->
+      @ctx.fillStyle = color
+      @ctx.font = font
+      @ctx.textAlign = align
+      @ctx.textBaseline = 'middle';
+      @ctx.measureText text
+    drawText: (text, position, rotate, center, color, font, align) ->
+      @ctx.fillStyle = color
+      @ctx.font = font
+      @ctx.textAlign = align
+      @ctx.textBaseline = 'middle';
+      @translate position, rotate, ->
+        @ctx.fillText text, -center.x, -center.y
+    milliseconds: ->
+      d = new Date
+      d.getTime()
+
+  class Rect
+    constructor: (x,y,w,h) ->
+      @pos = new Vector(x,y)
+      @size = new Vector(w,h)
 
   class Vector
-    constructor: (@x, @y) ->
+    constructor: (@x, @y, @z) ->
+      if !@z
+        @z = 0
+
+  class Text
+    constructor: (@text) ->
+      @position = new Vector 100, 100, 10
+      @color = '#fff'
+      @font = '20px sans-serif'
+      @align = 'left'
+      @rotate = 45
+      width = engine.measureText(@text, @color, @font, @align).width
+      @center = new Vector width / 2, 0
+    update: (delta) ->
+      @rotate++
+    draw: (engine) ->
+      engine.drawText @text, @position, @rotate, @center, @color, @font, @align
+
 
   class Sprite
-    constructor: (src) ->
+    constructor: (src, attrs) ->
       @image = new Image
       @loaded = false
       sprite = this
@@ -36,27 +91,77 @@ $(document).ready ->
       @h = 0
       @scale = 2
       @position = new Vector 100, 100
+      @center = new Vector 0, 0
       @image.onload = ->
         sprite.loaded = true
-        sprite.w = this.width
-        sprite.h = this.height
+        sprite.setSize @width, @height
       @image.src = src
+      if attrs
+        @sprites = new Vector attrs.sprites[0], attrs.sprites[1]
+      else
+        @sprites = [1,1]
+      @frame = 0
+      @setFPS 1
+      @frameTime = 0
+      @totalFrames = @sprites.x * @sprites.y
+      @rotate = 0
+
+    setFPS: (fps) ->
+      @frameRate = 1/fps;
+
+    setSize: (w,h) ->
+      @w = w / @sprites.x
+      @h = h / @sprites.y
+      @center.x = @w / 2 * @scale
+      @center.y = @h / 2 * @scale
+      @updatePos()
 
     update: (delta) ->
+      @updateCallback(delta) if @updateCallback
+      @nextFrame(delta)
+      @updatePos()
 
-    draw: (ctx) ->
+    updatePos: ->
+      @src = new Rect(@frame * @w, 0, @w, @h)
+      @dst = new Rect(@position.x, @position.y, @w * @scale, @h * @scale)
+
+    draw: (engine) ->
       return if not @loaded
-      ctx.drawImage @image, 0, 0, @w, @h, @position.x, @position.y, @w * @scale, @h * @scale
+      engine.drawImage @image, @src, @dst, @rotate, @center
+
+    nextFrame: (delta) ->
+      @frameTime += delta
+      if @frameTime > @frameRate
+        @frameTime -= @frameRate
+        @frame++
+        @frame = 0 if @frame >= @totalFrames
 
   window.engine = new Engine
   update = ->
     window.engine.update();
   window.setInterval update, 0.0333
 
-  sprite = new Sprite "sprite.png"
-  sprite.update = (delta) ->
+  sprite = new Sprite "anim.png", sprites:[3,1]
+  sprite.updateCallback = (delta) ->
+    @rotate += 5
+
+  sprite2 = new Sprite "anim.png", sprites:[3,1]
+  sprite2.position.y += 200
+  sprite2.setFPS 0.5
+  sprite2.updateCallback = (delta) ->
+    @rotate += 1
+
+  window.document.addEventListener 'keypress', (event) ->
+    window.console.log 'down'
+    char = String.fromCharCode event.keyCode
+    sprite.position.x += 1 if char == 'd'
+    true
+
+  text = new Text "foo"
 
   window.engine.add sprite
+  window.engine.add sprite2
+  window.engine.add text
 
   $("#pause").click ->
     window.engine.pause()
--- game.js
+++ game.js
@@ -2,7 +2,7 @@
   window.engine = null;
 
   $(document).ready(function() {
-    var Engine, Sprite, Vector, sprite, update;
+    var Engine, Rect, Sprite, Text, Vector, sprite, sprite2, text, update;
     Engine = (function() {
 
       function Engine() {
@@ -10,10 +10,12 @@
         this.ctx = this.screen.getContext("2d");
         this.w = this.screen.width;
         this.h = this.screen.height;
-        this.ctx.fillColor = '#fff';
+        this.clearColor = '#000';
+        this.ctx.fillStyle = this.clearColor;
         this.ctx.fillRect(0, 0, this.w, this.h);
         this.running = true;
         this.objects = [];
+        this.lastMillis = this.milliseconds();
       }
 
       Engine.prototype.add = function(object) {
@@ -21,44 +23,123 @@
       };
 
       Engine.prototype.update = function() {
-        var delta, o, _i, _j, _len, _len2, _ref, _ref2, _results;
+        var delta, o, _i, _j, _len, _len2, _ref, _ref2;
         if (!this.running) return;
-        delta = 0;
+        this.currentMillis = this.milliseconds();
+        delta = (this.currentMillis - this.lastMillis) / 1000;
         _ref = this.objects;
         for (_i = 0, _len = _ref.length; _i < _len; _i++) {
           o = _ref[_i];
           o.update(delta);
         }
+        this.ctx.fillStyle = this.clearColor;
         this.ctx.fillRect(0, 0, this.w, this.h);
         _ref2 = this.objects;
-        _results = [];
         for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
           o = _ref2[_j];
-          _results.push(o.draw(this.ctx));
+          o.draw(this);
         }
-        return _results;
+        return this.lastMillis = this.currentMillis;
       };
 
       Engine.prototype.pause = function() {
-        return this.running = !this.running;
+        this.running = !this.running;
+        return this.lastMillis = this.milliseconds();
+      };
+
+      Engine.prototype.translate = function(offset, rotate, callback) {
+        var angle;
+        angle = rotate * Math.PI / 180;
+        this.ctx.translate(offset.x, offset.y);
+        this.ctx.rotate(angle);
+        callback.apply(this);
+        this.ctx.rotate(-angle);
+        return this.ctx.translate(-offset.x, -offset.y);
+      };
+
+      Engine.prototype.drawImage = function(image, src, dst, rotate, center) {
+        return this.translate(dst.pos, rotate, function() {
+          return this.ctx.drawImage(image, src.pos.x, src.pos.y, src.size.x, src.size.y, -center.x, -center.y, dst.size.x, dst.size.y);
+        });
+      };
+
+      Engine.prototype.measureText = function(text, color, font, align) {
+        this.ctx.fillStyle = color;
+        this.ctx.font = font;
+        this.ctx.textAlign = align;
+        this.ctx.textBaseline = 'middle';
+        return this.ctx.measureText(text);
+      };
+
+      Engine.prototype.drawText = function(text, position, rotate, center, color, font, align) {
+        this.ctx.fillStyle = color;
+        this.ctx.font = font;
+        this.ctx.textAlign = align;
+        this.ctx.textBaseline = 'middle';
+        return this.translate(position, rotate, function() {
+          return this.ctx.fillText(text, -center.x, -center.y);
+        });
+      };
+
+      Engine.prototype.milliseconds = function() {
+        var d;
+        d = new Date;
+        return d.getTime();
       };
 
       return Engine;
 
     })();
+    Rect = (function() {
+
+      function Rect(x, y, w, h) {
+        this.pos = new Vector(x, y);
+        this.size = new Vector(w, h);
+      }
+
+      return Rect;
+
+    })();
     Vector = (function() {
 
-      function Vector(x, y) {
+      function Vector(x, y, z) {
         this.x = x;
         this.y = y;
+        this.z = z;
+        if (!this.z) this.z = 0;
       }
 
       return Vector;
 
     })();
+    Text = (function() {
+
+      function Text(text) {
+        var width;
+        this.text = text;
+        this.position = new Vector(100, 100, 10);
+        this.color = '#fff';
+        this.font = '20px sans-serif';
+        this.align = 'left';
+        this.rotate = 45;
+        width = engine.measureText(this.text, this.color, this.font, this.align).width;
+        this.center = new Vector(width / 2, 0);
+      }
+
+      Text.prototype.update = function(delta) {
+        return this.rotate++;
+      };
+
+      Text.prototype.draw = function(engine) {
+        return engine.drawText(this.text, this.position, this.rotate, this.center, this.color, this.font, this.align);
+      };
+
+      return Text;
+
+    })();
     Sprite = (function() {
 
-      function Sprite(src) {
+      function Sprite(src, attrs) {
         var sprite;
         this.image = new Image;
         this.loaded = false;
@@ -67,19 +148,59 @@
         this.h = 0;
         this.scale = 2;
         this.position = new Vector(100, 100);
+        this.center = new Vector(0, 0);
         this.image.onload = function() {
           sprite.loaded = true;
-          sprite.w = this.width;
-          return sprite.h = this.height;
+          return sprite.setSize(this.width, this.height);
         };
         this.image.src = src;
+        if (attrs) {
+          this.sprites = new Vector(attrs.sprites[0], attrs.sprites[1]);
+        } else {
+          this.sprites = [1, 1];
+        }
+        this.frame = 0;
+        this.setFPS(1);
+        this.frameTime = 0;
+        this.totalFrames = this.sprites.x * this.sprites.y;
+        this.rotate = 0;
       }
 
-      Sprite.prototype.update = function(delta) {};
+      Sprite.prototype.setFPS = function(fps) {
+        return this.frameRate = 1 / fps;
+      };
 
-      Sprite.prototype.draw = function(ctx) {
+      Sprite.prototype.setSize = function(w, h) {
+        this.w = w / this.sprites.x;
+        this.h = h / this.sprites.y;
+        this.center.x = this.w / 2 * this.scale;
+        this.center.y = this.h / 2 * this.scale;
+        return this.updatePos();
+      };
+
+      Sprite.prototype.update = function(delta) {
+        if (this.updateCallback) this.updateCallback(delta);
+        this.nextFrame(delta);
+        return this.updatePos();
+      };
+
+      Sprite.prototype.updatePos = function() {
+        this.src = new Rect(this.frame * this.w, 0, this.w, this.h);
+        return this.dst = new Rect(this.position.x, this.position.y, this.w * this.scale, this.h * this.scale);
+      };
+
+      Sprite.prototype.draw = function(engine) {
         if (!this.loaded) return;
-        return ctx.drawImage(this.image, 0, 0, this.w, this.h, this.position.x, this.position.y, this.w * this.scale, this.h * this.scale);
+        return engine.drawImage(this.image, this.src, this.dst, this.rotate, this.center);
+      };
+
+      Sprite.prototype.nextFrame = function(delta) {
+        this.frameTime += delta;
+        if (this.frameTime > this.frameRate) {
+          this.frameTime -= this.frameRate;
+          this.frame++;
+          if (this.frame >= this.totalFrames) return this.frame = 0;
+        }
       };
 
       return Sprite;
@@ -90,9 +211,31 @@
       return window.engine.update();
     };
     window.setInterval(update, 0.0333);
-    sprite = new Sprite("sprite.png");
-    sprite.update = function(delta) {};
+    sprite = new Sprite("anim.png", {
+      sprites: [3, 1]
+    });
+    sprite.updateCallback = function(delta) {
+      return this.rotate += 5;
+    };
+    sprite2 = new Sprite("anim.png", {
+      sprites: [3, 1]
+    });
+    sprite2.position.y += 200;
+    sprite2.setFPS(0.5);
+    sprite2.updateCallback = function(delta) {
+      return this.rotate += 1;
+    };
+    window.document.addEventListener('keypress', function(event) {
+      var char;
+      window.console.log('down');
+      char = String.fromCharCode(event.keyCode);
+      if (char === 'd') sprite.position.x += 1;
+      return true;
+    });
+    text = new Text("foo");
     window.engine.add(sprite);
+    window.engine.add(sprite2);
+    window.engine.add(text);
     return $("#pause").click(function() {
       window.engine.pause();
       if (window.engine.running) {
